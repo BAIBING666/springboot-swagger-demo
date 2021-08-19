13b498ae66c9558b3449fe5a31015dced4897422
BAIBING666 <1289769485@qq.com>
2021-08-19 13:20:17 +0800

    Initial commit

---
+++ src/main/java/com/swagger/springbootswaggerdemo/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.swagger.springbootswaggerdemo.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.naming.Name;
+
+/**
+ * @program: springboot-swagger-demo
+ * @description:
+ * @author: bing.bai
+ * @create: 2021-08-19 11:40
+ */
+@RestController
+@Api(
+    value = "用户相关接口",
+    tags = {"用户操作接口"})
+public class UserController {
+
+  /**
+   * @param name 姓名
+   * @return 用户信息
+   */
+  @RequestMapping("/getUser")
+  @ApiOperation(value = "获取用户信息", notes = "注意问题点 : name为必传")
+  public JSONObject getUser(@ApiParam(name = "name", value = "用户名") String name) {
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("user", name);
+    return jsonObject;
+  }
+}
